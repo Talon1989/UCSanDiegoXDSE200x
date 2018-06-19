e88781f0ce1adb62962cdb0292e175d759c3c0cb
Talon <asd@asd.com>
2018-06-19 12:57:48 +0100

    satellite end.

--- src/w-1/satelite/s1.py
+++ src/w-1/satelite/s1.py
@@ -50,6 +50,12 @@ def filterIntesity(photo):
     plt.show()
 
 
+def broadcastGrid():
+    arr1 = np.array([[0], [1], [2]])  # rows, [0], [1], [2]
+    arr2 = np.array([1, 2, 3])  # columns [1, 2, 3]
+    print(arr1 + arr2)
+
+
 def maskingImage(photo):
     assert isinstance(photo, imageio.core.util.Image)
     rows, cols, layers = photo.shape
@@ -61,20 +67,48 @@ def maskingImage(photo):
     # print('centerRow=', centerRow)
     # print('centerCol=', centerCol)
     dist_from_center = (X - centerRow) ** 2 + (Y - centerCol) ** 2
-    # print('dist_from_center=', dist_from_center)
+    # print('dist_from_center=', dist_from_center)  # [3725 by 4797]
     radius = (rows / 2) ** 2
     # print("Radius = ", radius)
     circular_mask = (dist_from_center > radius)
     # print(circular_mask)
     # print(circular_mask[1500:1700, 2000:2200])
     photo[circular_mask] = 0
+
+    halfUpper = X < centerRow
+    half_upper_mask = np.logical_and(halfUpper, circular_mask)
+    photo[half_upper_mask] = 200
+
     plt.figure(figsize=(15,15))
     plt.imshow(photo)
     plt.show()
 
 
+def highLightReds(photo):
+    assert isinstance(photo, imageio.core.util.Image)
+    red_mask = photo[:, :, 0] < 150  # True for all r values in photo, lower than 150
+    photo[red_mask] = 0
+    plt.figure(figsize=(15,15))
+    plt.imshow(photo)
+    plt.show()
+
+
+def rgbMask(photo):
+    assert isinstance(photo, imageio.core.util.Image)
+    red_mask = photo[:, :, 0] < 150
+    green_mask = photo[:, :, 1] > 100
+    blue_mask = photo[:, :, 2] < 100
+    final_mask = np.logical_and(red_mask, green_mask, blue_mask)
+    photo[final_mask] = 0
+    plt.figure(figsize=(15,15))
+    plt.imshow(photo)
+    plt.show()
+
+
+
+
 photo_data = imageio.imread('img/sd-3layers.jpg')
-maskingImage(photo_data)
+rgbMask(photo_data)
 
 
 
